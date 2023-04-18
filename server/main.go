2a9e1685b1fea0f05b6f3b484110862fd18e5c9c
kenya6565 <ken.australia0605@gmail.com>
2023-04-18 22:31:28 +0900

    add method ListFiles

--- server/main.go
+++ server/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"grpc-lesson/pb"
+	"io/ioutil"
 )
 
 type server struct {
@@ -10,5 +12,27 @@ type server struct {
 }
 
 func (*server) ListFiles(ctx context.Context, req *pb.ListFilesRequest) (*pb.ListFilesResponse, error) {
+	fmt.Println("ListFiles was invoked")
+	dir := "/Users/user/workspace/grpc-lesson/storage"
 
+	paths, err := ioutil.ReadDir(dir)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var filenames []string
+	for _, path := range paths {
+		// if path is not a directory, which is actually a file name
+		if !path.IsDir() {
+			// append file name to slice
+			filenames = append(filenames, path.Name())
+		}
+	}
+
+	// create ListFilesResponse as return val
+	res := &pb.ListFilesResponse{
+		Filenames: filenames,
+	}
+	return res, nil
 }
